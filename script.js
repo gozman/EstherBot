2bc225178dd6c358267ae1788b0b6a38b7af3ef4
mike gozzo <mgozzo@gmail.com>
2016-04-27 00:40:08 -0400

    add magical delays

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const Script = require('smooch-bot').Script;
 
 const scriptRules = require('./script.json');
 
+function wait(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
 module.exports = new Script({
     processing: {
         //prompt: (bot) => bot.say('Beep boop...'),
@@ -55,8 +61,9 @@ module.exports = new Script({
                     line = line.trim();
                     p = p.then(function() {
                         console.log(line);
-                        Thread.sleep(250)
-                        return bot.say(line);
+                        return wait(200).then(function() {
+                            return bot.say(line);
+                        });
                     });
                 });
 
